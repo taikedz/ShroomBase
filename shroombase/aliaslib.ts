5922b68702d3707761f23e873e2c145e4690108c
Tai Kedzierski <dch.tai@gmail.com>
2024-03-19 07:55:40 +0000

    Add alias lookup cache

--- shroombase/aliaslib.ts
+++ shroombase/aliaslib.ts
@@ -1,6 +1,7 @@
 import * as traverse from './traverse.ts'
 
 var ALL_ALIAS = null;
+var ALIAS_PATHS = {};
 
 export function registerAliases(data) {
     ALL_ALIAS = data;
@@ -8,7 +9,17 @@ export function registerAliases(data) {
 
 
 export function resolveAlias(alias) {
+
+    let value = ALIAS_PATHS[alias];
+    if (!( value === undefined)) { return value; }
+
     let tokens = alias.split("/");
-    return traverse.descend(ALL_ALIAS, tokens)
+    value = traverse.descend(ALL_ALIAS, tokens)
+
+    if (!(value === null || value === undefined)) {
+        ALIAS_PATHS[alias] = value;
+    }
+
+    return value;
 }
 
